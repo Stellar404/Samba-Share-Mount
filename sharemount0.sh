a184aaeab2dac5bd60f6c0369e721be6b1fc0abd
Stellar <init3@tuta.io>
2019-03-07 00:08:18 +0000

    sharemount0.sh

---
+++ sharemount0.sh
@@ -0,0 +1,8 @@
+#
+# -- Mount Samba Shared Drives CommandLine -- 
+#
+
+mount -t cifs -o user=XXXXXXXX //192.168.0.X/share0 /media/mount0
+mount -t cifs -o user=XXXXXXXX //192.168.0.X/share1 /media/mount1
+mount -t cifs -o user=XXXXXXXX //192.168.0.X/share2 /media/mount2
+mount -t cifs -o user=XXXXXXXX //192.168.0.X/share3 /media/mount3
