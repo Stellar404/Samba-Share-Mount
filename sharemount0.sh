a9339539f86ab50c8f55cc5712a0a577587a49b6
Stellar <init3@tuta.io>
2019-03-07 00:08:33 +0000

    Update sharemount0.sh

--- sharemount0.sh
+++ sharemount0.sh
@@ -6,3 +6,8 @@ mount -t cifs -o user=XXXXXXXX //192.168.0.X/share0 /media/mount0
 mount -t cifs -o user=XXXXXXXX //192.168.0.X/share1 /media/mount1
 mount -t cifs -o user=XXXXXXXX //192.168.0.X/share2 /media/mount2
 mount -t cifs -o user=XXXXXXXX //192.168.0.X/share3 /media/mount3
+
+#
+#
+#
+
